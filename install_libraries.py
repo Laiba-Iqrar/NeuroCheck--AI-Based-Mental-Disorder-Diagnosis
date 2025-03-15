8069f859e32a4446ca1624c9b391e7c64ef8bcc0
Laiba-Iqrar <laibaiqrarahmedkhan@gmail.com>
2025-03-16 02:07:06 +0500

    Add More dependencies

--- install_libraries.py
+++ install_libraries.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 
-packages = ['tensorflow' ,'pandas', 'numpy', 'scikit-fuzzy', 'scikit-learn', 'joblib']
+packages = ['tensorflow' ,'pandas', 'numpy', 'scikit-fuzzy', 'scikit-learn', 'joblib', 'flask']
 for package in packages:
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
